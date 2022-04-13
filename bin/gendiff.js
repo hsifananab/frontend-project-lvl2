6bb059d7713f1ee9c5081c540e194e0d725c57b8
hsifananab <yusov.yan@icloud.com>
2022-04-13 19:10:55 +0300

    add gendiff options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log('Hello!');
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --vers', 'output the current version');
+
+program.parse();
