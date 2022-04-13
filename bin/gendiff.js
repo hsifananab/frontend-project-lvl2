ee3dbb7ae428cae574ee0513c6f7b1dcd6641b3b
hsifananab <yusov.yan@icloud.com>
2022-04-13 19:17:54 +0300

    add gendiff format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,8 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-v, --vers', 'output the current version');
+  .version('0.0.1', '-v, --vers', 'output the current version')
+  .option('-f, --format', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
