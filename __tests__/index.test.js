48848d4c1fbf822b72f212c25ae742a2443ec211
hsifananab <yusov.yan@icloud.com>
2022-04-17 01:15:45 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,25 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../index.js';
 
-const filepath1 = '__fixtures__/file1.json';
-const filepath2 = '__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-test('gendiff', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(`{
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+const json1 = getFixturePath('file1.json');
+const json2 = getFixturePath('file2.json');
+
+const result = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`);
+}`;
+
+test('gendiff', () => {
+  expect(genDiff(json1, json2)).toEqual(result);
 });
