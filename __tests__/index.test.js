472785b3da523dd08300f3bf023bbdd07e02052c
hsifananab <yusov.yan@icloud.com>
2022-04-18 01:09:35 +0300

    add yaml tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,10 +6,14 @@ import genDiff from '../index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) =>
+const getPath = (filename) =>
   path.join(__dirname, '..', '__fixtures__', filename);
-const json1 = getFixturePath('file1.json');
-const json2 = getFixturePath('file2.json');
+
+const json1 = getPath('file1.json');
+const json2 = getPath('file2.json');
+
+const yaml1 = getPath('file1.yaml');
+const yaml2 = getPath('file2.yaml');
 
 const result = `{
   - follow: false
@@ -22,4 +26,5 @@ const result = `{
 
 test('gendiff', () => {
   expect(genDiff(json1, json2)).toEqual(result);
+  expect(genDiff(yaml1, yaml2)).toEqual(result);
 });
